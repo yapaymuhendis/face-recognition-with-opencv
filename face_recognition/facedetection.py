a5a1c8cd56077e62b867962dac364e5a8654aaea
cagan <109431950+yapaymuhendis@users.noreply.github.com>
2022-10-01 17:10:03 +0300

    Update facedetection.py

--- face_recognition/facedetection.py
+++ face_recognition/facedetection.py
@@ -1,27 +1,29 @@
 import face_recognition
 import cv2
 
-path = ("/Users/42cagan/Desktop/face_recognition/images/testimage.jpeg")
+path = ("images/testimage.jpeg")
 image = cv2.imread(path)
 
 faceLocations = face_recognition.face_locations(image)
-color = (0,255,0)
+# I am using the face_recognition library to detect faces in the image.
 
-# for döngüsüne alıp enumerate fonksiyonu kullanmamdaki amaç
-# yüzleri otomatik olarak kendisi bulsun diye. pt1 ve pt2'ye koordinatları söyleyip
-# cv2.rectangle'a giriyorum. kendisi buluyor döngü ile.
+color = (0,255,0)
+# I'm setting the color of the rectangle
+
 for index, faceloc in enumerate(faceLocations):
+    # The enumerate() method adds a counter to an iterable and returns it.
 
     topleftY,bottomrightX,bottomrightY,topleftX = faceloc
     pt1 = (topleftX,topleftY)
     pt2 = (bottomrightX,bottomrightY)
-
+    #I want it to determine its coordinates to throw a rectangle on the detected face, so I use it like this after I define the 4 places.
+    
     cv2.rectangle(image, pt1, pt2, color)
-    # Dikdörtgen çizer istediğin yere.
+    # Draw a rectangle wherever you want.
 
-    # q'ya bastığımda pencereyi kapat diyorum.
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+    # It stops working when I press the q key.    
 
 cv2.imshow("test", image)
 cv2.waitKey(0)
